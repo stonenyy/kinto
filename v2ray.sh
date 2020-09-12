53894f543aa45cdec0c70dfa872cd07f51bc89e3
elf <Nyy2@163.com>
2020-09-12 19:50:35 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -35,7 +35,7 @@ cat << EOF > /etc/config.json
                 "servers": [
                     {
                         "address": "127.0.0.1",
-                        "port": 9050
+                        "port": 9051
                     }
                 ]
             }
